a7cf0094d8a97d1b2c7efe8dc2c6bd6ae277a81c
Nikhil Rawal <nikhilrawal034@gmail.com>
2026-08-23 12:54:49 +0530

    fix: removed missing reader/index.html from vite build config

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        reader: resolve(__dirname, 'reader/index.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
